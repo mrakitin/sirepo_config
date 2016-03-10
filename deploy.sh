8b4653ea84ba04467cf8790b866ea86e514908c0
robnagler <github@q33.us>
2016-03-10 17:43:10 +0000

    start order incorrect

--- deploy.sh
+++ deploy.sh
@@ -64,7 +64,7 @@ fi
 #
 # Services
 #
-for s in celery-sirepo sirepo rabbitmq; do
+for s in rabbitmq celery-sirepo sirepo; do
     if ! systemctl status "$s" >& /dev/null; then
         systemctl enable "$s"
     fi
